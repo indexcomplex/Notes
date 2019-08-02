e584532f41a14627a1eee669e1d413f6ed171e1b
alan hernandez <indexcomplex@gmail.com>
2019-08-02 10:36:04 -0700

    Before connecting data source to table

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    var notes = ["hello", "bye"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    
+    @IBAction func didTapAdd(_ sender: Any) {
+        print("add")
+        
+    }
+    
+    
+    // to conform to UITableViewDataSource we need 2 func's
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notes.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        let note = notes[indexPath.row]
+        
+         cell.textLabel?.text = note
+        
+        return cell
+    }
+    
+    
+    
+    
+    
 }
 
