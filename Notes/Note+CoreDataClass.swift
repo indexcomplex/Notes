f0bf8274d5d4fb4a7e94195b1261cf7a8b394c12
alan hernandez <indexcomplex@gmail.com>
2019-08-06 16:33:59 -0700

    Working Notes and Core Data saving

---
+++ Notes/Note+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Note+CoreDataClass.swift
+//  Notes
+//
+//  Created by Alan Hernandez on 8/2/19.
+//  Copyright © 2019 alan Hernandez. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Note: NSManagedObject {
+
+}
---
+++ Notes/Note+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Note+CoreDataProperties.swift
+//  Notes
+//
+//  Created by Alan Hernandez on 8/2/19.
+//  Copyright © 2019 alan Hernandez. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Note {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Note> {
+        return NSFetchRequest<Note>(entityName: "Note")
+    }
+
+    @NSManaged public var body: String?
+
+}
